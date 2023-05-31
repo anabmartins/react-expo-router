735bd473192b8f6e8a80a5a6d36b7830f7045069
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-05-31 11:37:38 -0300

    new

---
+++ Tabs/app/_layout.tsx
@@ -0,0 +1,26 @@
+import { Tabs } from "expo-router";
+
+export default function AppLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          href: '/',
+        }}
+      />
+      <Tabs.Screen
+        name="telaA"
+        options={{
+          href: 'telaA',
+        }}
+      />
+      <Tabs.Screen
+        name="telaB"
+        options={{
+          href: 'telaB',
+        }}
+      />
+    </Tabs>
+  );
+}
